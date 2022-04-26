633c5dee32f90c62ccae6f6812b57ec782012794
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-04-26 16:37:49 -0700

    About to start FROSTT tensor conversion.

--- modules.sh
+++ modules.sh
@@ -1,3 +1,7 @@
+module load cmake
 module load python
 module load cudatoolkit
-conda activate gpu_linalg
+#conda activate gpu_linalg
+conda activate tensor_env
+export HDF5_USE_FILE_LOCKING=FALSE
+module load cray-hdf5

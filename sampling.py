657c1616f704afafa1f44b29cc31feee37560c5a
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-06-07 09:38:22 -0700

    In progress making changes to weight the samples so that the Leverage score sampler provides unbiased estimates.

--- sampling.py
+++ sampling.py
@@ -3,6 +3,7 @@ import numpy as np
 def get_samples(row_probs, num_samples):
 	row_range = list(range(len(row_probs)))
 	sample_idxs = np.random.choice(row_range, p=row_probs, size=num_samples) 
+	sampled_probs = row_probs[sample_idxs]
 
-	return sample_idxs
+	return sample_idxs, sampled_probs 
 

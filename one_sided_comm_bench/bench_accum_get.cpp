82f28119c1e7d3b1f022587a4d9f12cd43813a46
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-06-12 20:00:44 -0700

    Got everything to build!

--- one_sided_comm_bench/bench_accum_get.cpp
+++ one_sided_comm_bench/bench_accum_get.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <memory>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -27,36 +28,38 @@ void one_sided_mpi(
 	double* c_ptr = c.data();
 
 	for(int i = 0; i < rows; i++) {
-		for(int j = 0; j < r; j++) {
-			c_ptr[i * r + j] = a_ptr[i * r + j] * b_ptr[i * r + j];
+		for(int j = 0; j < cols; j++) {
+			c_ptr[i * cols + j] = a_ptr[i * cols + j] * b_ptr[i * cols + j];
 		}
 	}
 }
 
 
 int main(int argc, char** argv) {
-	int n = atoi(argv[1]);
-	string method(argv[2]);
-	int proc_count = atoi(argv[3]);
+	int rows = atoi(argv[1]);
+	int cols = atoi(argv[2]);
+	string method(argv[3]);
+	int proc_count = atoi(argv[4]);
 
 	std::random_device rd;  
     std::mt19937 gen(rd()); 
     std::uniform_real_distribution<> dis_real(-1.0, 1.0);
-    std::uniform_real_distribution<> dis_real(-1.0, 1.0);
 	std::uniform_int_distribution<> dis_int(0, proc_count - 1);
 
-	vector<double> a(n, 0.0);
-	vector<double> b(n, 0.0);
-	vector<double> c(n, 0.0);
-	vector<int> destinations(n, 0);
+	vector<double> a(rows * cols, 0.0);
+	vector<double> b(rows * cols, 0.0);
+	vector<double> c(rows * cols, 0.0);
+	vector<int> destinations(rows, 0);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < rows * cols; i++) {
 		a[i] = dis_real(gen);
 		b[i] = dis_real(gen);
 		c[i] = dis_real(gen);
-
-		destinations[i] = dis_int(gen);
     }
+
+    for (int i = 0; i < rows; i++) {
+		destinations[i] = dis_int(gen);
+	}
 
 	if(method == "one_sided_mpi") {
 		cout << "Benchmarking one-sided MPI!" << endl;

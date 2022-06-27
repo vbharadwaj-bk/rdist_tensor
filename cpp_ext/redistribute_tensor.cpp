8e8d06486d706ca93a7db033a6c4a3fb7e7a77e0
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-06-27 14:45:00 -0700

    Started moving over to a new file.

--- cpp_ext/redistribute_tensor.cpp
+++ cpp_ext/redistribute_tensor.cpp
@@ -8,20 +8,10 @@
 #include <mpi.h>
 
 #include "common.h"
+#include 
 
 using namespace std;
 namespace py = pybind11;
-
-/*
- * This is a bad prefix sum function.
- */
-void prefix_sum(vector<unsigned long long> &values, vector<unsigned long long> &offsets) {
-    unsigned long long sum = 0;
-    for(unsigned long long i = 0; i < values.size(); i++) {
-        offsets.push_back(sum);
-        sum += values[i];
-    }
-}
 
 /*
  * Count up the nonzeros in preparation to allocate receive buffers. 
@@ -48,18 +38,12 @@ void redistribute_nonzeros(
     int dim = prefix_mult.info.shape[0];
 
     // Initialize AlltoAll data structures 
-    vector<unsigned long long> send_counts(proc_count, 0);
-    vector<unsigned long long> recv_counts(proc_count, 0);
-
-    vector<unsigned long long> send_offsets;
-    vector<unsigned long long> recv_offsets;
-    vector<unsigned long long> running_offsets;
-
+    vector<uint64_t> send_counts(proc_count, 0);
     vector<int> processor_assignments(nnz, -1);
 
     // TODO: Could parallelize using OpenMP if we want faster IO 
-    for(unsigned long long i = 0; i < nnz; i++) {
-        unsigned long long processor = 0;
+    for(uint64_t i = 0; i < nnz; i++) {
+        uint64_t processor = 0;
         for(int j = 0; j < dim; j++) {
             processor += prefix_mult.ptr[j] * (coords.ptrs[j][i] / intervals.ptr[j]); 
         }
@@ -67,97 +51,18 @@ void redistribute_nonzeros(
         processor_assignments[i] = processor;
     }
 
-    MPI_Alltoall(send_counts.data(), 
-                1, MPI_UNSIGNED_LONG_LONG, 
-                recv_counts.data(), 
-                1, MPI_UNSIGNED_LONG_LONG, 
-                MPI_COMM_WORLD);
-
-    unsigned long long total_received_coords = 
-				std::accumulate(recv_counts.begin(), recv_counts.end(), 0);
-
-    prefix_sum(recv_counts, recv_offsets);
-
-    vector<vector<unsigned long long>> send_idx;
-    vector<double> send_values(nnz, 0.0);
-
-    allocate_recv_buffers(dim, total_received_coords, recv_idx_py, recv_values_py);
-    NumpyList<unsigned long long> recv_idx(recv_idx_py);
-    NumpyList<double> recv_values(recv_values_py);
-
-    for(int i = 0; i < dim; i++) {
-        send_idx.emplace_back(nnz, 0);
-    }
-
-    // Pack the send buffers
-    prefix_sum(send_counts, send_offsets);
-    running_offsets = send_offsets;
-
-    for(unsigned long long i = 0; i < nnz; i++) {
-        int owner = processor_assignments[i];
-        unsigned long long idx;
-
-        // #pragma omp atomic capture 
-        idx = running_offsets[owner]++;
-
-        for(int j = 0; j < dim; j++) {
-            send_idx[j][idx] = coords.ptrs[j][i];
-        }
-        send_values[idx] = values.ptr[i]; 
-    }
-
-    // Execute the AlltoAll operations
-
-    // ===================================================
-    // Explicit downcast: this errors if we have more than one
-    // integer's worth of data to transfer. We should replace this 
-    // with a more intelligent AlltoAll operation 
-
-    unsigned long long total_send_coords = 
-				std::accumulate(send_counts.begin(), send_counts.end(), 0);
-
-    vector<int> send_counts_dcast;
-    vector<int> send_offsets_dcast;
-    vector<int> recv_counts_dcast;
-    vector<int> recv_offsets_dcast;
-
-    if(total_send_coords >= INT32_MAX || total_received_coords >= INT32_MAX) {
-        cout << "ERROR, ALL_TO_ALL BUFFER_SIZE EXCEEDED!" << endl;
-        exit(1);
-    }
-    else {
-        for(uint i = 0; i < proc_count; i++) {
-            send_counts_dcast.push_back((int) send_counts[i]);
-            send_offsets_dcast.push_back((int) send_offsets[i]);
-
-            recv_counts_dcast.push_back((int) recv_counts[i]);
-            recv_offsets_dcast.push_back((int) recv_offsets[i]);
-        } 
-    }
-
-    // ===================================================
-
-    for(int j = 0; j < dim; j++) {
-        MPI_Alltoallv(send_idx[j].data(), 
-                        send_counts_dcast.data(), 
-                        send_offsets_dcast.data(), 
-                        MPI_UNSIGNED_LONG_LONG, 
-                        recv_idx.ptrs[j], 
-                        recv_counts_dcast.data(), 
-                        recv_offsets_dcast.data(), 
-                        MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD 
-                        );
-    }
-    
-    MPI_Alltoallv(send_values.data(), 
-                    send_counts_dcast.data(), 
-                    send_offsets_dcast.data(), 
-                    MPI_DOUBLE, 
-                    recv_values.ptrs[0], 
-                    recv_counts_dcast.data(), 
-                    recv_offsets_dcast.data(), 
-                    MPI_DOUBLE, MPI_COMM_WORLD 
-                    );
+    redistribute_nonzeros(
+		dim,
+		proc_count,
+		nnz,
+        coords,
+        values,
+		processor_assignments,
+		send_counts,
+        recv_idx_py,
+        recv_values_py,
+        allocate_recv_buffers 
+    );
 }
 
 PYBIND11_MODULE(redistribute_tensor, m) {
--- cpp_ext/tensor_alltoallv.h
+++ cpp_ext/tensor_alltoallv.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
@@ -10,3 +12,138 @@
 
 using namespace std;
 namespace py = pybind11;
+
+/*
+ * This is a bad prefix sum function.
+ */
+void prefix_sum(vector<unsigned long long> &values, vector<unsigned long long> &offsets) {
+    unsigned long long sum = 0;
+    for(unsigned long long i = 0; i < values.size(); i++) {
+        offsets.push_back(sum);
+        sum += values[i];
+    }
+}
+
+/*
+ * Note: sendcounts could be computed from 
+ * processor_assignments, but we require precomputation
+ * for efficiency. 
+ */
+void redistribute_nonzeros(
+		int dim,
+		uint64_t proc_count,
+		uint64_t nnz,
+        NumpyList<uint64_t> &coords,
+        NumpyArray<double> &values,
+		vector<int> &processor_assignments,
+		vector<uint64_t> &send_counts,
+        py::list &recv_idx_py,
+        py::list &recv_values_py,
+        py::function &allocate_recv_buffers 
+        ) {
+    vector<uint64_t> recv_counts(proc_count, 0);
+
+    vector<uint64_t> send_offsets;
+    vector<uint64_t> recv_offsets;
+    vector<uint64_t> running_offsets;
+
+    NumpyList<uint64_t> recv_idx(recv_idx_py);
+    NumpyList<double> recv_values(recv_values_py);
+
+    vector<uint64_t> send_offsets;
+    vector<uint64_t> recv_offsets;
+    vector<uint64_t> running_offsets;
+
+    MPI_Alltoall(send_counts.data(), 
+                1, MPI_UNSIGNED_LONG_LONG, 
+                recv_counts.data(), 
+                1, MPI_UNSIGNED_LONG_LONG, 
+                MPI_COMM_WORLD);
+
+    uint64_t total_received_coords = 
+				std::accumulate(recv_counts.begin(), recv_counts.end(), 0);
+
+    prefix_sum(recv_counts, recv_offsets);
+
+    vector<vector<uint64_t>> send_idx;
+    vector<double> send_values(nnz, 0.0);
+
+    allocate_recv_buffers(dim, total_received_coords, recv_idx_py, recv_values_py);
+    NumpyList<uint64_t> recv_idx(recv_idx_py);
+    NumpyList<double> recv_values(recv_values_py);
+
+    for(int i = 0; i < dim; i++) {
+        send_idx.emplace_back(nnz, 0);
+    }
+
+    // Pack the send buffers
+    prefix_sum(send_counts, send_offsets);
+    running_offsets = send_offsets;
+
+    for(uint64_t i = 0; i < nnz; i++) {
+        int owner = processor_assignments[i];
+        uint64_t idx;
+
+        // #pragma omp atomic capture 
+        idx = running_offsets[owner]++;
+
+        for(int j = 0; j < dim; j++) {
+            send_idx[j][idx] = coords.ptrs[j][i];
+        }
+        send_values[idx] = values.ptr[i]; 
+    }
+
+    // Execute the AlltoAll operations
+
+    // ===================================================
+    // Explicit downcast: this errors if we have more than one
+    // integer's worth of data to transfer. We should replace this 
+    // with a more intelligent AlltoAll operation 
+
+    uint64_t total_send_coords = 
+				std::accumulate(send_counts.begin(), send_counts.end(), 0);
+
+    vector<int> send_counts_dcast;
+    vector<int> send_offsets_dcast;
+    vector<int> recv_counts_dcast;
+    vector<int> recv_offsets_dcast;
+
+    if(total_send_coords >= INT32_MAX || total_received_coords >= INT32_MAX) {
+        cout << "ERROR, ALL_TO_ALL BUFFER_SIZE EXCEEDED!" << endl;
+        exit(1);
+    }
+    else {
+        for(uint i = 0; i < proc_count; i++) {
+            send_counts_dcast.push_back((int) send_counts[i]);
+            send_offsets_dcast.push_back((int) send_offsets[i]);
+
+            recv_counts_dcast.push_back((int) recv_counts[i]);
+            recv_offsets_dcast.push_back((int) recv_offsets[i]);
+        } 
+    }
+
+    // ===================================================
+
+    for(int j = 0; j < dim; j++) {
+        MPI_Alltoallv(send_idx[j].data(), 
+                        send_counts_dcast.data(), 
+                        send_offsets_dcast.data(), 
+                        MPI_UNSIGNED_LONG_LONG, 
+                        recv_idx.ptrs[j], 
+                        recv_counts_dcast.data(), 
+                        recv_offsets_dcast.data(), 
+                        MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD 
+                        );
+    }
+    
+    MPI_Alltoallv(send_values.data(), 
+                    send_counts_dcast.data(), 
+                    send_offsets_dcast.data(), 
+                    MPI_DOUBLE, 
+                    recv_values.ptrs[0], 
+                    recv_counts_dcast.data(), 
+                    recv_offsets_dcast.data(), 
+                    MPI_DOUBLE, MPI_COMM_WORLD 
+                    );
+
+}

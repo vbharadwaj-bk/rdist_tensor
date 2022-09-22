623f6581ce363f0910a7ebe9c30fa80ca031447e
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-09-22 15:30:06 -0700

    Added 4-node benchmark for Reddit.

--- baseline/splatt_run_script.sh
+++ baseline/splatt_run_script.sh
@@ -1,6 +1,14 @@
-TENSOR_NAME=amazon-reviews
-#TENSOR_NAME=reddit-2015
+#TENSOR_NAME=amazon-reviews
+TENSOR_NAME=reddit-2015
 
-$SPLATT_LOC/splatt cpd \
+#$SPLATT_LOC/splatt cpd \
+#        $BIN_TENSOR_LOC/$TENSOR_NAME.bin \
+#        -r 25 -t 128 --nowrite 
+
+
+#$SPLATT_LOC/splatt check \
+#        $BIN_TENSOR_LOC/$TENSOR_NAME.bin
+
+srun -N 4 -n 512 $SPLATT_LOC/splatt cpd \
         $BIN_TENSOR_LOC/$TENSOR_NAME.bin \
-        -r 25 -t 128 --nowrite 
+        -r 25 -t 1 --nowrite 

Fixed gram matrix computation.

test_modules.py
@@ -11,8 +11,8 @@ def test_grid():
     proc_dims = np.array([2, 2, 2], dtype=np.int32)
     grid = Grid(proc_dims)
     tensor_grid = TensorGrid([300, 201, 105], grid)
-    low_rank_tensor = LowRankTensor(25, tensor_grid)
-    low_rank_tensor.compute_gram_matrices()
+    low_rank_tensor = LowRankTensor(5, tensor_grid)
+    low_rank_tensor.test_gram_matrix_computation()
 
 if __name__=='__main__':
     num_procs = MPI.COMM_WORLD.Get_size()
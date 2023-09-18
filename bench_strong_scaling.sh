cfd612458344ef1441f275288ea29fec81cadde3
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2023-09-18 11:54:46 -0700

    Major improvements to the strong scaling bash script.

--- bench_strong_scaling.sh
+++ bench_strong_scaling.sh
@@ -1,23 +1,28 @@
 . env.sh
 
 export TRIAL_COUNT=1
-export OMP_NUM_THREADS=16
-
 export TENSOR=uber
 export ITERATIONS=20
 export RANK=100
+export OMP_NUM_THREADS=16
 
-for ALG in cp_arls_lev 
+export CORES_PER_NODE=128
+export RANKS_PER_NODE=$((CORES_PER_NODE / OMP_NUM_THREADS))
+
+for NODE_COUNT in 1
 do
-    for (( trial=1; trial<=$TRIAL_COUNT; trial++ )) 
+    for ALG in sts_cp 
     do
-        srun -N 1 -n 8 -c 32 python decompose.py -i $TENSOR \
-                    --trank $RANK \
-                    -s 65536 \
-                    -iter $ITERATIONS \
-                    -alg $ALG \
-                    -dist accumulator_stationary \
-                    -o data/strong_scaling \
-                    -r $TRIAL_COUNT
+        for (( trial=1; trial<=$TRIAL_COUNT; trial++ )) 
+        do
+            srun -N $NODE_COUNT -n $((NODE_COUNT * RANKS_PER_NODE)) -c $((OMP_NUM_THREADS * 2)) python decompose.py -i $TENSOR \
+                        --trank $RANK \
+                        -s 65536 \
+                        -iter $ITERATIONS \
+                        -alg $ALG \
+                        -dist accumulator_stationary \
+                        -o data/strong_scaling \
+                        -r $TRIAL_COUNT
+        done
     done
 done
